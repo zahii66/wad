ede0c9c890ff4af548bbeba6504aaaac88122657
Zahii66 <ztayyab0175@ucp.edu.pk>
2019-05-03 10:58:34 +0500

    delete button

--- lec 15/script.js
+++ lec 15/script.js
@@ -14,7 +14,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("user-input");
 var ul = document.getElementsByTagName("ul")[0];
 var listItems = document.getElementsByTagName("li");
-
+var deleteItem = document.getElementsByClassName("del");
 function inputLength(){
     if(input.value.length > 0 )
         return true;
@@ -22,14 +22,30 @@ function inputLength(){
 }
 function createListItem(){
     var li = document.createElement("li");
+    var btn = document.createElement("button");
     li.append(document.createTextNode(input.value));
+    btn.append(document.createTextNode("Delete"));
+    btn.classList.add("del");
+    li.append(btn);
     ul.append(li);
     input.value = '';
+    addDoneListener();
+    deleteDoneListener();
 }
 function addItemAfterClick(){
         if(inputLength()) {
             createListItem();
         }
+}
+function addDoneListener() {
+    for(var i=0;i<listItems.length;i++){
+        listItems[i].addEventListener("click",isDone);
+    }
+}
+function deleteDoneListener() {
+    for(var i=0;i<deleteItem.length;i++){
+        deleteItem[i].addEventListener("click",deleteItems);
+    }
 }
 function addItemAfterPress(e) {
     if(inputLength() && e.which === 13) {
@@ -39,13 +55,17 @@ function addItemAfterPress(e) {
 function isDone(){
     this.classList.toggle("done");
 }
+function deleteItems(){
+    this.parentElement.remove();
+}
 button.addEventListener("click",addItemAfterClick);
 input.addEventListener("keypress",addItemAfterPress);
 for(var i=0;i<listItems.length;i++){
     listItems[i].addEventListener("click",isDone);
 }
 
-
+addDoneListener();
+deleteDoneListener();
 
 
 
